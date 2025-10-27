cff57863a1e3e7930e069a61cf3793ec31e90de2
userfromhuy <userfromhuy@gmail.com>
2025-10-27 15:59:42 +0000

    DS

--- src/widgets/SettingsMenu/store/UserSettingsStore.ts
+++ src/widgets/SettingsMenu/store/UserSettingsStore.ts
@@ -3,6 +3,7 @@ import { ref } from "vue";
 import { UserInterfaceState } from "../types/SettingsType.ts";
 
 export const useUserSettingsStore = defineStore('user settings', () => {
+
   const mode = ref<UserInterfaceState['mode']['value']>('casual')
   const color = ref<UserInterfaceState['color']['value']>('gray')
   return { mode, color }
